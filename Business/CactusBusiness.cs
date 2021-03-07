4b6be331af787c1fd2291f00daa9e24b6d2d94e0
Моника Джелепова <monikadzhelepova03@pmg-haskovo.org>
2021-03-07 11:19:41 +0200

    Update cactusbusiness

---
+++ Business/CactusBusiness.cs
@@ -0,0 +1,67 @@
+﻿using Data;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business
+{
+    public class CactusBusiness
+    {
+        private GardenContext context;
+
+        public CactusBusiness(GardenContext gardenContext)
+        {
+            context = gardenContext;
+        }
+
+        public CactusBusiness()
+        {
+            context = new GardenContext();
+        }
+
+        /// <summary>
+        /// Gives all cactus in the database.
+        /// </summary>
+        public List<Cactus> GetAllFlowers()
+        {
+            return context.Cactuses.ToList();
+        }
+
+        /// <summary>
+        /// Adds cactus in database.
+        /// </summary>
+        public void Add(Cactus cactus)
+        {
+            context.Cactuses.Add(cactus);
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Updates cactus.
+        /// </summary>
+        public void Update(Cactus cactus)
+        {
+            var item = context.Cactuses.Find(cactus.Id);
+            if (item != null)
+            {
+                context.Entry(item).CurrentValues.SetValues(cactus);
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Deletes a cactus with wanted id.
+        /// </summary>
+        public void Delete(int id)
+        {
+            var item = context.Cactuses.FirstOrDefault(m => m.Id == id);
+            if (item != null)
+            {
+                context.Cactuses.Remove(item);
+                context.SaveChanges();
+            }
+        }
+    }
+}
