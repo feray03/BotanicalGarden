4d3a581005c88acd3cab2ebcd2844e620665d6d9
Feray <Feray@DESKTOP-IVQQUDH>
2021-03-07 10:54:06 +0200

    FlowerBusiness-update

---
+++ Business/FlowerBusiness.cs
@@ -0,0 +1,67 @@
+﻿using Data;
+using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business
+{
+    public class FlowerBusiness
+    {
+        private GardenContext context;
+
+        public FlowerBusiness(GardenContext gardenContext)
+        {
+            context = gardenContext;
+        }
+
+        public FlowerBusiness()
+        {
+            context = new GardenContext();
+        }
+
+        /// <summary>
+        /// Gives all flowers in the database.
+        /// </summary>
+        public List<Flower> GetAllFlowers()
+        {
+            return context.Flowers.ToList();
+        }
+
+        /// <summary>
+        /// Adds flower in database.
+        /// </summary>
+        public void Add(Flower flower)
+        {
+            context.Flowers.Add(flower);
+            context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Updates flower.
+        /// </summary>
+        public void Update(Flower flower)
+        {
+            var item = context.Flowers.Find(flower.Id);
+            if (item != null)
+            {
+                context.Entry(item).CurrentValues.SetValues(flower);
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Deletes a flower with wanted id.
+        /// </summary>
+        public void Delete(int id)
+        {
+            var item = context.Flowers.FirstOrDefault(m => m.Id == id);
+            if (item != null)
+            {
+                context.Flowers.Remove(item);
+                context.SaveChanges();
+            }
+        }
+    }
+}
