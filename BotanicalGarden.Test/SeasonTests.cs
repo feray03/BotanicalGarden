69f9db6418399e9c9669136b073a8ac8f5dc611a
Feray <Feray@DESKTOP-IVQQUDH>
2021-03-08 23:20:55 +0200

    ShrubTests-final SeasonTests-final

---
+++ BotanicalGarden.Test/SeasonTests.cs
@@ -0,0 +1,70 @@
+﻿using Business;
+using Data;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BotanicalGarden.Test
+{
+    public class SeasonTests
+    {
+        [TestCase]
+        public void Gives_All_Seasons()
+        {
+            var data = new List<Season>
+            {
+                new Season { Name="First" },
+                new Season { Name="Second" },
+                new Season { Name="Third" },
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Season>>();
+            mockSet.As<IQueryable<Season>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Season>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Season>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Season>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<GardenContext>();
+            mockContext.Setup(c => c.Seasons).Returns(mockSet.Object);
+
+            var business = new SeasonBusiness(mockContext.Object);
+            var Seasons = business.GetAllSeasons();
+
+            Assert.AreEqual(3, Seasons.Count);
+            Assert.AreEqual("First", Seasons[0].Name);
+            Assert.AreEqual("Second", Seasons[1].Name);
+            Assert.AreEqual("Third", Seasons[2].Name);
+        }
+
+        [TestCase]
+        public void Gives_Season_By_Name()
+        {
+            var data = new List<Season>()
+            {
+                new Season{Id=1, Name="Season1"},
+                new Season{Id=2, Name="Season2"},
+                new Season{Id=3, Name="Season3"},
+                new Season{Id=4, Name="Season4"},
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Season>>();
+            mockSet.As<IQueryable<Season>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Season>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Season>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Season>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<GardenContext>();
+            mockContext.Setup(c => c.Seasons).Returns(mockSet.Object);
+
+            var business = new SeasonBusiness(mockContext.Object);
+
+            var season = business.GetSeasonByName("Season1");
+            Assert.AreEqual("Season1", season.Name);
+        }
+    }
+}
--- BotanicalGarden.Test/ShrubTests.cs
+++ BotanicalGarden.Test/ShrubTests.cs
@@ -40,5 +40,72 @@ namespace BotanicalGarden.Test
             Assert.AreEqual("Second", Shrubs[1].Name);
             Assert.AreEqual("Third", Shrubs[2].Name);
         }
+
+        [TestCase]
+        public void Add_Shrub()
+        {
+            var mockSet = new Mock<DbSet<Shrub>>();
+            var shrub = new Shrub();
+            var mockContext = new Mock<GardenContext>();
+            mockContext.Setup(m => m.Shrubs).Returns(mockSet.Object);
+
+            var business = new ShrubBusiness(mockContext.Object);
+            business.Add(shrub);
+
+            mockSet.Verify(m => m.Add(It.IsAny<Shrub>()), Times.Once());
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+
+        [TestCase]
+        public void Gives_Shrub_By_Name()
+        {
+            var data = new List<Shrub>()
+            {
+                new Shrub{Id=1, Name="Shrub1"},
+                new Shrub{Id=2, Name="Shrub2" },
+                new Shrub{Id=3, Name="Shrub3"},
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Shrub>>();
+            mockSet.As<IQueryable<Shrub>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Shrub>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Shrub>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Shrub>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<GardenContext>();
+            mockContext.Setup(c => c.Shrubs).Returns(mockSet.Object);
+
+            var business = new ShrubBusiness(mockContext.Object);
+
+            var shrub = business.GetShrubByName("Shrub1");
+            Assert.AreEqual("Shrub1", shrub.Name);
+        }
+
+        [TestCase]
+        public void Remove_Shrub()
+        {
+            var data = new List<Shrub>()
+            {
+                new Shrub{Id=1, Name="Shrub1"},
+                new Shrub{Id=2, Name="Shrub2" },
+                new Shrub{Id=3, Name="Shrub3"},
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Shrub>>();
+            mockSet.As<IQueryable<Shrub>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Shrub>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Shrub>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Shrub>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<GardenContext>();
+            mockContext.Setup(x => x.Shrubs).Returns(mockSet.Object);
+
+            var business = new ShrubBusiness(mockContext.Object);
+            var shrubs = business.GetAllShrubs();
+
+            int deletedShrubId = 1; business.Delete(shrubs[0].Id);
+
+            Assert.IsNull(business.GetAllShrubs().FirstOrDefault(x => x.Id == deletedShrubId));
+        }
     }
 }
